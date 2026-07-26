9b5848827f6df6232579639840beffb8b6cd486b
rahulltelkar <rahul.friends262@gmail.com>
2026-07-26 11:40:41 +0000

    Add Terraform remote backend with S3 and DynamoDB

---
+++ terraform/backend-bootstrap.tf
@@ -0,0 +1,47 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "rahul-platform-tfstate-003107248036-dev"
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "terraform-state"
+    }
+  )
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "rahul-terraform-state-lock"
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "terraform-lock"
+    }
+  )
+}
